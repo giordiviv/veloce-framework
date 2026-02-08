5c1cb7411c3eede256c85e17c0e15b3189d70d1e
giordanoviviani-epfl <94621888+giordanoviviani-epfl@users.noreply.github.com>
2026-02-08 22:47:06 +0100

    Added notebook and src directories.
    src directory contains the velocefw package under the velocefw directory. I have
    added an __init__.py file and a fits.py file with initial functions to read
    veloce data.  notebook directory contains a veloceIII_framework.ipynb notebook
    where I show how the analysis for VELOCE III was conducted.

---
+++ src/velocefw/__init__.py
@@ -0,0 +1,16 @@
+"""Veloce Framework.
+
+A Python package for handling astronomical data and building reconstructing
+frameworks. The package was developed during the analysis of VELOCE-III to
+build a framework to reconstruct galactic classical Cepheids RV curves based on
+the models published in VELOCE (Anderson, Viviani et al. 2024).
+
+The package was then generalized to be used for similar analysis and expanding
+the initial published framework, as well as to allow new users a rapid and
+hassle-free access to the original VELOCE-III framework.
+
+"""
+
+from .fits import get_header, get_table
+
+__all__ = ["get_header", "get_table"]
---
+++ src/velocefw/fits.py
@@ -0,0 +1,128 @@
+"""Module to handle fits files."""
+
+import logging
+from pathlib import Path
+from typing import Any
+
+import pandas as pd
+from astropy.io import fits
+from astropy.table import Table
+
+logger = logging.getLogger(__name__)
+
+
+def get_header(
+    file: str | Path,
+    hdu: int = 0,
+) -> dict[str, Any]:
+    """Get the primary header of a fits file.
+
+    Parameters
+    ----------
+    file : str or Path
+        Path to the fits file.
+    hdu : int, optional
+        HDU number to read.
+        Default is 0, which corresponds to the primary HDU.
+
+    Returns
+    -------
+    dict[str, Any]
+        Dictionary containing the primary header.
+
+    """
+    file = Path(file)
+    if not file.exists():
+        message = f"File {file} does not exist."
+        logger.error(message)
+        raise FileNotFoundError(message)
+
+    try:
+        with fits.open(file) as hdul:  # type: ignore[no-untyped-call]
+            base_hdu: fits.PrimaryHDU = hdul[hdu]  # pyright: ignore[reportAssignmentType]
+            header = dict(base_hdu.header)
+    except Exception as e:
+        logger.exception(
+            "Error reading primary header from: %s",
+            file,
+            extra={
+                "exception": e,
+            },
+        )
+        raise
+
+    logger.debug("Successfully read primary header from: %s", file)
+    return header
+
+
+def get_table(
+    file: str | Path,
+    hdu: int,
+    columns: list[str] | None = None,
+) -> pd.DataFrame:
+    """Get a table from a fits file and return it as a pandas.DataFrame.
+
+    If the fits file contains a PrimaryHDU, the columns argument is ignored and
+    all columns are read. If the fits file contains a TableHDU, only the
+    specified columns are read. If a column contains multi-dimensional data, it
+    is split into multiple columns in the resulting dataframe.
+
+    Parameters
+    ----------
+    file : str or Path
+        Path to the fits file.
+    hdu : int
+        HDU table number to read.
+    columns : list[str], optional
+        List of columns to read from the fits file.
+
+    Returns
+    -------
+    pd.DataFrame
+        Pandas dataframe containing the data from the fits file.
+
+    """
+    file = Path(file)
+    if not file.exists():
+        message = f"File {file} does not exist."
+        logger.error(message)
+        raise FileNotFoundError(message)
+
+    with fits.open(file) as hdul:  # type: ignore[no-untyped-call]
+        base_hdu: fits.TableHDU = hdul[hdu]  # type: ignore[attr-defined]
+
+        if isinstance(base_hdu, fits.PrimaryHDU):
+            if columns is not None:
+                logger.warning("Columns argument is ignored for PrimaryHDU.")
+            table = Table(base_hdu.data)
+            dataframe = table.to_pandas()
+        else:
+            if columns is None:
+                columns = base_hdu.columns.names  # type: ignore[attr-defined]
+
+            table = Table(base_hdu.data)
+            table.keep_columns(columns)
+
+            # Check for multi-dimensional columns and split them into multiple
+            # columns if necessary
+            column_names = table.columns.copy()
+            for col in column_names:
+                if len(table[col].shape) > 1:  # type: ignore[attr-defined]
+                    logger.warning("Column %s has shape %s", col, table[col].shape)  # type: ignore[attr-defined]
+                    logger.warning("Splitting the column into multiple columns.")
+                    for i in range(table[col].shape[1]):  # type: ignore[attr-defined]
+                        table.add_column(table[col][:, i], name=f"{col}_{i}", index=-1)  # type: ignore[attr-defined]
+                    table.remove_column(col)  # type: ignore[attr-defined]
+
+            try:
+                dataframe = table.to_pandas()
+            except ValueError as e:
+                logger.exception(
+                    "Error reading data from: %s",
+                    file,
+                    extra={"exception": e},
+                )
+                raise
+
+    logger.debug("Successfully read dataframe from: %s", file)
+    return dataframe
